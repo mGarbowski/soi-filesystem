9c224d7bb02c78c4ed02c94669cb2153b1ecea02
mgarbowski <mikolaj.garbowski@gmail.com>
2024-01-13 16:26:55 +0100

    moved constants to a separate file

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,8 @@
 #include <fstream>
 #include <cstring>
 #include <vector>
-#include <cmath>
 
-#define BLOCK_SIZE (32 * 1024) // 32kB
-#define N_INODES 256
-#define N_BLOCKS 4096  // for total disk capacity of 128MB
-#define DIRECTORY_SIZE 2048  // BLOCK_SIZE / sizeof(DirectoryEntry
-#define FILENAME_SIZE 15
-#define INODE_BLOCKS 16
-#define MAX_FILE_SIZE (INODE_BLOCKS * BLOCK_SIZE)
-
-//TODO enum
-#define TYPE_FILE 0
-#define TYPE_DIRECTORY 1
+#include "constants.h"
 
 int64_t getCurrentTimestamp() {
     return std::chrono::system_clock::now().time_since_epoch().count();
