d3271d4c3a365407aa333e0431c7f4166326f883
mgarbowski <mikolaj.garbowski@gmail.com>
2024-01-13 16:47:23 +0100

    split definitions

--- src/Superblock.cpp
+++ src/Superblock.cpp
@@ -1,5 +1,17 @@
-//
-// Created by mgarbowski on 13.01.24.
-//
-
 #include "Superblock.h"
+
+Superblock::Superblock() {
+    this->filesystemCreatedTimestamp = getCurrentTimestamp();
+    this->diskSize = BLOCK_SIZE * N_BLOCKS;
+    this->nFreeBlocks = N_BLOCKS;
+    this->nFiles = 0;
+    this->blockSize = BLOCK_SIZE;
+}
+
+void Superblock::printInfo() const {
+    std::cout << "Filesystem created at " << this->filesystemCreatedTimestamp << std::endl;
+    std::cout << "Disk size " << this->diskSize << std::endl;
+    std::cout << "Block size " << this->blockSize << std::endl;
+    std::cout << "Number of free blocks " << this->nFreeBlocks << std::endl;
+    std::cout << "Number of files " << (int) this->nFiles << std::endl;
+}
--- src/Superblock.h
+++ src/Superblock.h
@@ -11,21 +11,9 @@ struct Superblock {
     uint32_t nFreeBlocks;
     uint8_t nFiles;
 
-    Superblock() {
-        this->filesystemCreatedTimestamp = getCurrentTimestamp();
-        this->diskSize = BLOCK_SIZE * N_BLOCKS;
-        this->nFreeBlocks = N_BLOCKS;
-        this->nFiles = 0;
-        this->blockSize = BLOCK_SIZE;
-    }
+    Superblock();
 
-    void printInfo() const {
-        std::cout << "Filesystem created at " << this->filesystemCreatedTimestamp << std::endl;
-        std::cout << "Disk size " << this->diskSize << std::endl;
-        std::cout << "Block size " << this->blockSize << std::endl;
-        std::cout << "Number of free blocks " << this->nFreeBlocks << std::endl;
-        std::cout << "Number of files " << (int) this->nFiles << std::endl;
-    }
+    void printInfo() const;
 };
 
 
